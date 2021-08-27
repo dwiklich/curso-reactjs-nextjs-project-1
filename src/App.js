6b9e8be238cdfa3580c3b009d5998ff5ae72b334
dwiklich <d.wiklich@gmail.com>
2021-08-27 18:22:33 -0300

    Lifecycle Methods Part 1

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { Component } from "react";
 
 class App extends Component {
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
-        title: "O titulo 1",
+        title: "O titulo 1111",
         body: "O corpo 1",
       },
       {
@@ -32,12 +33,52 @@ class App extends Component {
       },
     ],
   };
+  timeOutUpdate = null;
+
+  handleTimeOut = () => {
+    setTimeout(() => {
+      const { posts, counter } = this.state;
+      posts[0].title = "O título mudou";
+
+      console.log("Montado componente");
+      // console.log(`counter: ${this.state.counter}`);
+
+      this.setState({
+        counter: counter + 1,
+        posts: [...posts],
+      });
+    }, 2000);
+  };
+
+  // componentDidMount -> atualiza uma vez o estado do componente assim que renderiza
+  componentDidMount() {
+    // setInterval fica atualizando o componente com o tempo inserido na function setInterval. Se transforma num compomentDidUpdate
+    // setInterval(() => {
+    //   console.log("Componente atulizando a cada 2 segundos");
+    // }, 2000);
+
+    this.handleTimeOut();
+
+    console.log("Componente apos handleTimeOut");
+  }
+
+  // componentDidUpdate -> atualiza o estado do componente a cada segundo
+  componentDidUpdate() {
+    this.handleTimeOut();
+  }
+
+  // componentWillUnmount -> executa toda vez que o componente for criado ou atualizado
+  componentWillUnmount() {
+    // limpa o cache do react pois pode ficar cache e bugar o componente
+    clearTimeout(this.timeOutUpdate);
+  }
 
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
+        <p>counter: {counter}</p>
         {posts.map((post) => (
           <div key={post.id}>
             <h1>{post.title}</h1>
