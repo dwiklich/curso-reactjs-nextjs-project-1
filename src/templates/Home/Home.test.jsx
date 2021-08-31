5dbc00cbac6c57b043b4ded13cbda35ab5f75892
dwiklich <d.wiklich@gmail.com>
2021-08-31 18:30:10 -0300

    Configurando Mock Service Worker para iniciar os testes

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -1,11 +1,86 @@
-import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+
 import { Home } from './index';
 
+const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title handler 1',
+          body: 'body handler 1',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title handler 2',
+          body: 'body handler 2',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title handler 3',
+          body: 'body handler 3',
+        },
+        {
+          userId: 4,
+          id: 4,
+          title: 'title handler 4',
+          body: 'body handler 4',
+        },
+      ]),
+    );
+  }),
+  rest.get('https://jsonplaceholder.typicode.com/photos', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          url: 'img/img-handler-1.jpg',
+        },
+        {
+          url: 'img/img-handler-2.jpg',
+        },
+        {
+          url: 'img/img-handler-3.jpg',
+        },
+        {
+          url: 'img/img-handler-4.jpg',
+        },
+        {
+          url: 'img/img-handler-5.jpg',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  test('is a dummy test', () => {
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => {
+    server.close();
+  });
+
+  test('should render search, posts and load more', async () => {
     // expect(1).toBe(1);
-    render(<Home />);
-    const linkElement = screen.getByText('Load more posts');
-    expect(linkElement).toBeInTheDocument();
+    const { debug } = render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts =x');
+    expect(noMorePosts).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(noMorePosts);
+    debug();
+    // const linkElement = screen.debug('Load more posts');
+    // expect(linkElement).toBeInTheDocument();
   });
 });
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -43,7 +43,6 @@ export const Home = () => {
   };
 
   useEffect(() => {
-    console.log('Oi');
     handleLoadPosts(0, postsPerPage);
   }, [handleLoadPosts, postsPerPage]);
 
@@ -65,83 +64,3 @@ export const Home = () => {
     </section>
   );
 };
-
-// export class Home2 extends Component {
-//   state = {
-//     posts: [],
-//     allPosts: [],
-//     page: 0,
-//     postsPerPage: 10,
-//     searchValue: "",
-//     componentMontado: 0,
-//     componentAtualizado: 0,
-//   };
-
-//   async componentDidMount() {
-//     await this.loadPosts();
-//   }
-
-//   loadPosts = async () => {
-//     const { postsPerPage } = this.state;
-
-//     const postsAndPhotos = await loadPosts();
-//     this.setState({
-//       posts: postsAndPhotos.slice(0, postsPerPage),
-//       allPosts: postsAndPhotos,
-//     });
-//   };
-
-//   loadMorePosts = () => {
-//     const { page, postsPerPage, allPosts, posts } = this.state;
-//     const nextPage = page + postsPerPage;
-//     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-
-//     this.setState({
-//       page: nextPage,
-//       posts: [...posts, ...nextPosts],
-//     });
-//   };
-
-//   handleChange = (e) => {
-//     const { value } = e.target;
-//     this.setState({ searchValue: value });
-//   };
-
-//   render() {
-//     const { posts, allPosts, page, postsPerPage, searchValue } = this.state;
-//     const noMorePosts = page + postsPerPage >= allPosts.length;
-
-//     const filteredPosts = !!searchValue
-//       ? allPosts.filter((post) => {
-//           return post.title.toLowerCase().includes(searchValue.toLowerCase());
-//         })
-//       : posts;
-
-//     return (
-//       <section className="container">
-//         <div className="search-container">
-//           {!!searchValue && <h1>Search value: {searchValue}</h1>}
-//           <label htmlFor="pesquisa" className="search-pesquisa">
-//             <TextInput
-//               handleChange={this.handleChange}
-//               searchValue={searchValue}
-//             />
-//           </label>
-//         </div>
-
-//         {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
-//         {filteredPosts.length === 0 && <p>Não existem posts =x </p>}
-
-//         <div className="button-container">
-//           {!searchValue && (
-//             <Button
-//               disabled={noMorePosts}
-//               onClick={this.loadMorePosts}
-//               text="Load more posts"
-//             />
-//           )}
-//         </div>
-//       </section>
-//     );
-//   }
-// }
